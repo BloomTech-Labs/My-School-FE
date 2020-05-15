77ff5d3691436970ecf5f7e57bedbd06d6a72d7d
Elysia Gabe <elysiagabe@gmail.com>
2020-05-14 21:57:03 -0700

    Creates DateSelector component and builds out basic form elements

---
+++ my-school/src/Components/DateSelector.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import moment from 'moment';
+import {
+    FormControl,
+    FormLabel,
+    Select,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
+} from '@chakra-ui/core';
+
+const DateSelector = () => {
+    // Sets default date values based on current date
+    const d = new Date();
+    const currentMonth = d.getMonth() + 1; 
+    const currentDate = d.getDate();
+    const currentYear = d.getFullYear();
+
+    // Uses Moment Library to create months array with month number, name & length (# of days)
+    const rangeStart = moment().startOf('year');
+    const rangeEnd = moment().endOf('year');
+
+    const months = [];
+
+    let dateIterator = moment(rangeStart);
+    while (dateIterator.isBefore(rangeEnd) || dateIterator.isSame(rangeEnd)) {
+        const endOfMonth = moment(dateIterator).endOf('month');
+        months.push({
+            num: dateIterator.month() + 1,
+            length: endOfMonth.date(),
+            name: dateIterator.format('MMMM')
+        });
+        dateIterator.add(1, 'month');
+    }
+    
+    // Month state
+    const [month, setMonth] = useState(months.find(m => m.num === currentMonth));
+
+    const handleMonthChange = e => {
+        setMonth(months.find(m => m.num == e.target.value))
+    }
+
+    return (
+        <>
+            <FormControl>
+                <FormLabel htmlFor="month">Month</FormLabel>
+                <Select id="month" defaultValue={month ? month.num : 1} onChange={handleMonthChange}>
+                    {months.map(month => {
+                        return (
+                            <option value={month.num} key={month.num}>{month.name}</option>
+                        )
+                    })}
+                </Select>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel htmlFor="day">Day</FormLabel>
+                <NumberInput id="day" defaultValue={currentDate} min={1} max={month ? month.length : 31}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel htmlFor="year">Year</FormLabel>
+                <NumberInput id="year" defaultValue={currentYear} max={currentYear + 10} min={currentYear - 100}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </FormControl>
+        </>
+    )
+}
+
+export default DateSelector;
--- my-school/src/Components/Forms/AddActivityForm.js
+++ my-school/src/Components/Forms/AddActivityForm.js
@@ -6,10 +6,18 @@ import {
     Input,
     Select,
     Textarea,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
     Button,
-  } from "@chakra-ui/core";
+} from "@chakra-ui/core";
+import DateSelector from '../DateSelector';
 
 const AddActivityForm = () => {
+
+
     return (
         <form>
             {/* Title, Subject, Description, Duration, Submission Date, Upload Photo */}
@@ -37,8 +45,34 @@ const AddActivityForm = () => {
                 <Textarea placeholder="Tell us all about what you did in this activity!" />
             </FormControl>
 
-            
+            <p>How long did it take to complete this activity?</p>
+            <p>Duration</p>
+            <FormControl>
+                <FormLabel htmlFor="hours">Hours</FormLabel>
+                <NumberInput id="hours" defaultValue={0}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </FormControl>
 
+            <FormControl>
+                <FormLabel htmlFor="minutes">Minutes</FormLabel>
+                <NumberInput id="minutes" defaultValue={0} max={59}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </FormControl>
+
+            <p>Confirm Submission Date</p>
+            <DateSelector />
+
+            <Button type="submit">Submit</Button>
         </form>
     )
 }
