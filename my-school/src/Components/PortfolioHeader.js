49ec1e4a022425f8147eef1655036f798e229da0
unknown <sara.reidy.21@gmail.com>
2020-05-21 16:49:31 -0400

    Updated header and nav to reflect figma doc where functionality exists

--- my-school/src/Components/PortfolioHeader.js
+++ my-school/src/Components/PortfolioHeader.js
@@ -1,46 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Heading } from '@chakra-ui/core';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button, Heading, Flex, Icon } from "@chakra-ui/core";
+import axios from "axios";
 
 const PortfolioHeader = () => {
+  const [title, setTitle] = useState("");
 
-    const [ title, setTitle ] = useState('');
+  useEffect(() => {
+    axios
+      .get("https://my-school-v1.herokuapp.com/api/users/3")
+      .then((response) => {
+        if (response.user_type_id === 1) {
+          setTitle([response.data.name] + "'s Portfolio");
+        } else {
+          setTitle("My Portfolio");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get("https://my-school-v1.herokuapp.com/api/users/3")
-        .then(response => {
-            
-            if(response.user_type_id === 1){
-                setTitle([response.data.name]+"'s Portfolio")
-            } else {
-                setTitle("My Portfolio")
-            };
-        })
-        .catch(error => {
-           console.log(error) })
-    }, []);
-    
-    return(
-         
-        
-      
-        <header>
-            {/* TITLE  -- based on user type and name*/}
-            <Link to = '/portfolio'><Heading as="h2">{title}</Heading></Link>
+  return (
+    <Flex margin="2% 4%" justify="space-between">
+      {/* TITLE  -- based on user type and name*/}
+      <Link to="/portfolio">
+        <Heading as="h2">{title}</Heading>
+      </Link>
+      <Flex width="25%" justify="space-evenly">
+        <Link to="/add">
+          <Button leftIcon="add" variantColor="teal" variant="solid">
+            Add Activity
+          </Button>
+        </Link>
 
-            {/* ADD ACTIVITY BUTTON */}
-            <Link to ='/add'><Button lefticon="small-add" variantColor="teal" variant="solid">Add Activity</Button></Link>
+        {/*  EXPORT BUTTON -- PARENTS ONLY? */}
+        <Link to="/doc">
+          <Button leftIcon="download" variantColor="teal" variant="solid">
+            Convert to PDF
+          </Button>
+        </Link>
+      </Flex>
 
-            {/*  EXPORT BUTTON -- PARENTS ONLY? */}
-            <Link to='/doc' ><Button lefticon="download" variantColor="teal" variant="solid">Convert to PDF</Button></Link>
-        {/*  Start with opening in another tab, then download. */}
-            {/*SEARCH BOX AND SORT/FILTER FEATUREs WILL GO HERE*/}
-            {/* future -- activity tracker? */}
-        </header> 
-      
-    )
-}
+      {/*  Start with opening in another tab, then download. */}
+      {/*SEARCH BOX AND SORT/FILTER FEATUREs WILL GO HERE*/}
+      {/* future -- activity tracker? */}
+    </Flex>
+  );
+};
 
 export default PortfolioHeader;
