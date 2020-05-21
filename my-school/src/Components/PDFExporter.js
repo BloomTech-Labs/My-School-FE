57479c68c350937a75ea2c65dda6d7b5c6e5ab8b
Katrina Hernandez <abqkatrina@gmail.com>
2020-05-21 12:07:35 -0600

    initial styling

--- my-school/src/Components/PDFExporter.js
+++ my-school/src/Components/PDFExporter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Page,
   Text,
@@ -7,56 +7,66 @@ import {
   StyleSheet,
   Image,
   BlobProvider
-} from "@react-pdf/renderer";
-import {Link} from "react-router-dom";
-import axios from "axios";
+} from "@react-pdf/renderer";;
 
-// import ActivityCard from "./ActivityCard";
-
-// Create styles
-const styles = StyleSheet.create({
+// ----------------------STYLES-----------------------------------------
+const style = StyleSheet.create({
   body: {
     paddingTop: 35,
     paddingBottom: 65,
     paddingHorizontal: 35,
+    backgroundColor: "white",
   },
   page: {
-    flexDirection: "row",
+    flexFlow: "row wrap",
     backgroundColor: "#E4E4E4",
   },
   section: {
     margin: 10,
     padding: 10,
     flexGrow: 1,
+    alignSelf: "right",
+    width: 200,
+  },
+  header: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'Times-Roman'
+  },
+  subtitle: {
+    fontSize: 18,
+    margin: 12,
+    fontFamily: 'Times-Roman'
   },
   text: {
-    margin: 12,
+    margin: 16,
     fontSize: 14,
-    textAlign: "justify",
-    fontFamily: "Times-Roman",
+    textAlign: 'justify',
+    fontFamily: 'Times-Roman'
   },
   image: {
+    marginVertical: 15,
+    marginHorizontal: 100,
     width: '150px'
   },
 });
-
-
-
+// ----------------------/STYLES-----------------------------------------
 // Create Document Component
 const MyDocument = ({ activities }) => {
 
-  const student = [activities.studentsName] + "'s Portfolio";
-
   const PdfPortfolio = (
-    <Document title={student}>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section} >
+    <Document style={style.doc} title={""}>
+      <Page size="A4" style={style.page}>
+        <View >
           {activities.map((a) => {
             return (
-              <View key={a.id}>
-                <Text>{a.name}</Text>
-                <Text>{a.description}</Text>
-                {a.photo && <Image src={a.photo} style={styles.image}/>}
+              <View key={a.id} className='section'>
+                <Text style={style.header}>{a.name}</Text>
+                <Text style={style.subtitle}>Date: {a.completion_date}  Subject: {a.subject} Duration:{a.duration}"</Text>
+              <Text style={style.text}>{a.description}</Text>
+                {a.photo && <Image src={a.photo} style={style.image} />}
               </View>
             );
           })}
