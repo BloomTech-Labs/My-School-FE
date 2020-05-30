83cbac027091643d699d409f4bd53040e93cd15a
Elysia Gabe <elysiagabe@gmail.com>
2020-05-29 17:14:54 -0700

    Adds switch and organizes App.js imports

--- my-school/src/App.js
+++ my-school/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import TopNav from "./Components/Dashboards/Nav/TopNav";
-import MainContainer from "./Components/Dashboards/MainContainer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import customTheme from "./Styles/theme";
 import ReactGA from "react-ga";
+//Components
 import Landing from "./Components/EnterUser/Landing";
 import AuthPage from "./Components/EnterUser/AuthPage";
-import AddActivityForm from "./Components/Portfolio/Activity/AddActivity/AddActivityForm";
+import MainLayout from "./Components/MainLayout";
+import MainContainer from "./Components/Dashboards/MainContainer";
 import PortfolioContainer from "./Components/Portfolio/PortfolioContainer";
+import AddActivityForm from "./Components/Portfolio/Activity/AddActivity/AddActivityForm";
 import PDFExporter from './Components/Portfolio/PDFExporter'
 import ActivityOverview from "./Components/Portfolio/Activity/ActivityOverview";
 import StudentRegistration from './Components/studentRegister';
-import MainLayout from "./Components/MainLayout";
 
 function App() {
   React.useEffect((_) => {
@@ -26,37 +26,39 @@ function App() {
       <CSSReset />
       <div className="App">
         <Router >
-          <Route exact path='/'>
-            <Landing />
-          </Route>
-          <Route path = '/login'>
-            <AuthPage login={true} />
-          </Route>
-          <Route path='/signup'>
-            <AuthPage login={false} />
-          </Route>
-          <Route path='/dashboard'>
-            <MainLayout page={<MainContainer />} />
-          </Route>
-          <Route path='/settings'>
-            {/*Account settings*/}
-            {/* <MainLayout page={PUT PAGE HERE} /> */}
-          </Route>
-          <Route exact path='/portfolio/:id'>
-            <MainLayout page={<PortfolioContainer />} />
-          </Route>
-          <Route path='/portfolio/:id/add'>
-            <MainLayout page={<AddActivityForm />} />
-          </Route>
-          <Route path='/portfolio/:id/export'>
-            <MainLayout page={<PDFExporter />} />
-          </Route>
-          <Route path='/activity/:id'>
-            <MainLayout page={<ActivityOverview />} />
-          </Route>
-          <Route path='/addstudent'>
-            <MainLayout page={<StudentRegistration/>} />
-          </Route>
+          <Switch>
+            <Route exact path='/'>
+              <Landing />
+            </Route>
+            <Route path = '/login'>
+              <AuthPage login={true} />
+            </Route>
+            <Route path='/signup'>
+              <AuthPage login={false} />
+            </Route>
+            <Route path='/dashboard'>
+              <MainLayout page={<MainContainer />} />
+            </Route>
+            <Route path='/settings'>
+              {/*Account settings*/}
+              {/* <MainLayout page={PUT PAGE HERE} /> */}
+            </Route>
+            <Route exact path='/portfolio/:id'>
+              <MainLayout page={<PortfolioContainer />} />
+            </Route>
+            <Route path='/portfolio/:id/add'>
+              <MainLayout page={<AddActivityForm />} />
+            </Route>
+            <Route path='/portfolio/:id/export'>
+              <MainLayout page={<PDFExporter />} />
+            </Route>
+            <Route path='/activity/:id'>
+              <MainLayout page={<ActivityOverview />} />
+            </Route>
+            <Route path='/addstudent'>
+              <MainLayout page={<StudentRegistration/>} />
+            </Route>
+          </Switch>
         </Router>
       </div>
     </ThemeProvider>
--- my-school/src/Components/MainLayout.js
+++ my-school/src/Components/MainLayout.js
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import TopNav from './Dashboards/Nav/TopNav';
 
 const MainLayout = props => {
     const Page = props.page; 
-
-    useEffect(() => {
-        console.log("main layout mounts")
-    }, [])
 
     return (
         <>
