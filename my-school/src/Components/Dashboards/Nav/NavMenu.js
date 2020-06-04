c6e343f67f4b29d51a0043d1b982589ea4bd8e64
Elysia Gabe <elysiagabe@gmail.com>
2020-06-04 16:23:20 -0700

    merge updates

--- my-school/src/Components/Dashboards/Nav/NavMenu.js
+++ my-school/src/Components/Dashboards/Nav/NavMenu.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getFamilyName } from '../../../actions/actions-users';
 import {
@@ -27,13 +27,13 @@ const NavMenu = ({ user, family, isLoading, err, getFamilyName }) => {
 
   useEffect(() => {
     getFamilyName(user.family_id)
-  }, [getFamilyName, user, family])
+  }, [user, family])
 
   const handleAdminSettings = () => {
     history.push('/settings')
   }
 
-  const manageStudent =(e) => { 
+  const manageStudent = (e) => {
     e.preventDefault();
     const id = e.target.value;
     history.push(`/settings/${id}`)
@@ -49,57 +49,58 @@ const NavMenu = ({ user, family, isLoading, err, getFamilyName }) => {
     localStorage.clear();
     history.push('/login');
   }
-  
+
   return (
     // <>Test</>
-      <Menu>
-        {/* MenuButton is the trigger to open the MenuList */}
-        <MenuButton as={Button} bg='transparent' color="black" variantColor='btnBlue'>
-          <NavName user={user}/>
-          <Avatar size="md" src={user.profile_picture} alt="Your profile picture" ml="16px" border="2px" borderColor="lightblue" />
-        </MenuButton>
-        {/* MenuList is the wrapper for MenuItems */}
-        <MenuList p="0" mx="24px" w="315px" boxShadow="0px 40px 80px rgba(0, 0, 0, 0.12)">
+    <Menu>
+      {/* MenuButton is the trigger to open the MenuList */}
+      <MenuButton as={Button} bg='transparent' color="black" variantColor='btnBlue'>
+        <NavName user={user} />
+        <Avatar size="md" src={user.profile_picture} alt="Your profile picture" ml="16px" border="2px" borderColor="lightblue" />
+      </MenuButton>
+      {/* MenuList is the wrapper for MenuItems */}
+      <MenuList p="0" mx="24px" w="315px" boxShadow="0px 40px 80px rgba(0, 0, 0, 0.12)">
 
-          {/* ACCOUNT INFO & SETTINGS */}
-          <Flex flexDirection="row" w="100%" pt="24px" px="24px" bg="white" align="center">
-              <Avatar size="md" src={user.profile_picture} alt="Your profile picture" border="2px" borderColor="myschoolblue" mr="12px"/>
-              <Flex flexDirection="column">
-                <Text fontSize="1.125" fontWeight="bold" color="myschoolblue">{user.name !== null ? `${user.name}` : `Your Account`}</Text>
-                <Text fontSize="0.625rem" color="gray.600" textTransform="uppercase">{user.user_type_id === 1 ? `Primary Account` : `Student Account`}</Text>
-              </Flex>
-            </Flex>
-            {user.user_type_id === 1 && <MenuItem fontSize="sm" color="gray.700" px="24px" mt="12px" py="12px" onClick={handleAdminSettings}>Account Settings</MenuItem>}
+        {/* ACCOUNT INFO & SETTINGS */}
+        <Flex flexDirection="row" w="100%" pt="24px" px="24px" bg="white" align="center">
+          <Avatar size="md" src={user.profile_picture} alt="Your profile picture" border="2px" borderColor="myschoolblue" mr="12px" />
+          <Flex flexDirection="column">
+            <Text fontSize="1.125" fontWeight="bold" color="myschoolblue">{user.name !== null ? `${user.name}` : `Your Account`}</Text>
+            <Text fontSize="0.625rem" color="gray.600" textTransform="uppercase">{user.user_type_id === 1 ? `Primary Account` : `Student Account`}</Text>
+          </Flex>
+        </Flex>
+        {user.user_type_id === 1 && <MenuItem fontSize="sm" color="gray.700" px="24px" mt="12px" py="12px" onClick={handleAdminSettings}>Account Settings</MenuItem>}
 
-          <MenuDivider color="gray.300" m="0" />
+        <MenuDivider color="gray.300" m="0" />
 
-          {/* LIST OF STUDENT ACCOUNTS IF PARENT ACCOUNT */}
-          {user.user_type_id === 1 && 
-            <>
+        {/* LIST OF STUDENT ACCOUNTS IF PARENT ACCOUNT */}
+        {user.user_type_id === 1 &&
+          <>
             {/* STUDENT ACCOUNT MANAGEMENT */}
             <MenuGroup title={`${user.familyName} Family`} fontSize="1.125" color="gray.800" fontWeight="bold" mx="24px" mt="20px" mb="12px" p="0">
               {/* EXISTING STUDENT ACCOUNTS */}
-              {family.length > 1 ? 
-              family.map(s => {
-                // family includes parent, so much filter for student accts only
-                if (s.user_type_id === 2) {
-                return (
-                  <MenuItem key={s.id} value={s.id} py="8px" pl="24px" onClick={manageStudent}>
-                    <Avatar size="sm" src={s.profile_picture} alt={`${s.name} profile picture`} />
-                    <Flex flexDirection="column" ml="12px">
-                      <Text fontSize="md" fontWeight="bold" color="gray.700">{s.name}</Text>
-                      <Text fontSize="0.625rem" color="gray.500" textTransform="uppercase">Student Account</Text>
-                    </Flex>
-                    <Box fontSize="0.625rem" color="myschoolblue" textTransform="uppercase" border="1px" borderColor="myschoolblue" borderRadius="4px" h="24px" w="56px" bg="white" ml="52px">
-                      <Text pt="4px" pl="5px">Manage</Text>
-                    </Box>
-                  </MenuItem>
-                )} else {
-                  return null
-                }
-              })
-              
-              : <Text fontSize="sm" color="gray.700" py="8px" my="12px" px="24px">No students have been added to your family yet</Text> } 
+              {family.length > 1 ?
+                family.map(s => {
+                  // family includes parent, so much filter for student accts only
+                  if (s.user_type_id === 2) {
+                    return (
+                      <MenuItem key={s.id} value={s.id} py="8px" pl="24px" onClick={manageStudent}>
+                        <Avatar size="sm" src={s.profile_picture} alt={`${s.name} profile picture`} />
+                        <Flex flexDirection="column" ml="12px">
+                          <Text fontSize="md" fontWeight="bold" color="gray.700">{s.name}</Text>
+                          <Text fontSize="0.625rem" color="gray.500" textTransform="uppercase">Student Account</Text>
+                        </Flex>
+                        <Box fontSize="0.625rem" color="myschoolblue" textTransform="uppercase" border="1px" borderColor="myschoolblue" borderRadius="4px" h="24px" w="56px" bg="white" ml="52px">
+                          <Text pt="4px" pl="5px">Manage</Text>
+                        </Box>
+                      </MenuItem>
+                    )
+                  } else {
+                    return null
+                  }
+                })
+
+                : <Text fontSize="sm" color="gray.700" py="8px" my="12px" px="24px">No students have been added to your family yet</Text>}
 
               {/* ADD STUDENT */}
               <MenuItem onClick={handleAddStudent} mt="8px" mb="56px" px="24px" py="8px">
@@ -107,15 +108,15 @@ const NavMenu = ({ user, family, isLoading, err, getFamilyName }) => {
                 <Text fontSize="0.625rem" color="myschoolorange" fontWeight="bold" textTransform="uppercase" ml="12px">Add New</Text>
               </MenuItem>
             </MenuGroup>
-            </>
-          }
-          {/* SIGN OUT */}
-          <MenuItem bg="gray.100" h="70px" onClick={handleLogout}>
-            <Text w="95%" fontSize="sm" fontWeight="bold" color="gray.700" textTransform="uppercase">Sign Out</Text>
-            <Image src={LogoutIcon} alt="Sign out icon" w="1.125rem" display="block"/>
-          </MenuItem>
-        </MenuList>
-      </Menu>
+          </>
+        }
+        {/* SIGN OUT */}
+        <MenuItem bg="gray.100" h="70px" onClick={handleLogout}>
+          <Text w="95%" fontSize="sm" fontWeight="bold" color="gray.700" textTransform="uppercase">Sign Out</Text>
+          <Image src={LogoutIcon} alt="Sign out icon" w="1.125rem" display="block" />
+        </MenuItem>
+      </MenuList>
+    </Menu>
   )
 };
 
