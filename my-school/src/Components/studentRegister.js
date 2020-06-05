643500f83e17d97d70c487b8ddc6c6a1bc070d16
Elysia Gabe <elysiagabe@gmail.com>
2020-06-04 17:56:06 -0700

    Removes FormContext (not necessary here)

--- my-school/src/Components/studentRegister.js
+++ my-school/src/Components/studentRegister.js
@@ -1,5 +1,5 @@
-import React,{ useRef } from 'react';
-import { useForm, FormContext } from 'react-hook-form';
+import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 import { connect } from 'react-redux';
 import { getFamilyName } from '../actions/actions-users';
@@ -17,8 +17,6 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
 const StudentRegister = props => {
-
-    const methods = useForm();
     const toast = useToast();
     const history = useHistory();
     const { register, handleSubmit, errors, watch } = useForm();
@@ -32,154 +30,154 @@ const StudentRegister = props => {
             password: data.password,
             family_id: localStorage.getItem('family_id'),
             name: data.firstname,
-            user_type_id: 2, 
+            user_type_id: 2,
         }
         //this needs to be axios with auth once we get the auth middleware up in the backend
         axios.post(`https://my-school-v1.herokuapp.com/api/auth/registration`, studentUser)
-        .then(res => {
-            toast({
-                title: "Account created.",
-                description: `"We've created your account for ${data.firstname}.`,
-                status: "success",
-                duration: 9000,
-                isClosable: true,
-              })
-              history.push(`/dashboard/${localStorage.getItem('userId')}`)
-              props.getFamilyName(localStorage.getItem('family_id'))
-        })
-        .catch(err => console.log(err.message))
+            .then(res => {
+                toast({
+                    title: "Account created.",
+                    description: `"We've created your account for ${data.firstname}.`,
+                    status: "success",
+                    duration: 9000,
+                    isClosable: true,
+                })
+                history.push(`/dashboard/${localStorage.getItem('userId')}`)
+                props.getFamilyName(localStorage.getItem('family_id'))
+            })
+            .catch(err => console.log(err.message))
     };
 
     const validateCredentials = (value) => {
-        if(value <= 0 ){
+        if (value <= 0) {
             return 'must provide credentials'
-        }else{
+        } else {
             return null
         }
     };
 
     const pushHistory = () => {
         history.push('/dashboard')
-      }
+    }
 
     return (
         <>
-        <Flex  flexDirection='column' p='1.5rem 0rem 3rem 10rem'>
-            <Text paddingBottom='3.5rem' fontSize="1.125rem" fontWeight="700" color="gray.800"><span color='blue.900' className='link'onClick={pushHistory}>Dashboard</span> / Add a new acoount</Text>
-            <h3 style={{ fontWeight:'bold'}}>New account setup</h3>
-            <FormContext {...methods} >
-                <form onSubmit={handleSubmit(handleStudentRegister)}> 
+            <Flex flexDirection='column' p='1.5rem 0rem 3rem 10rem'>
+                <Text paddingBottom='3.5rem' fontSize="1.125rem" fontWeight="700" color="gray.800"><span color='blue.900' className='link' onClick={pushHistory}>Dashboard</span> / Add a new acoount</Text>
+                <h3 style={{ fontWeight: 'bold' }}>New account setup</h3>
+
+                <form onSubmit={handleSubmit(handleStudentRegister)}>
                     <Flex w='80vw' flexWrap='wrap' p='1.5rem 1.5rem 1.5rem 0rem' h='50vh' data-testid='form-submit'>
                         <FormControl isInvalid={errors.username} w='39%'>
                             <FormLabel htmlFor="username">Username</FormLabel>
-                            <Input 
+                            <Input
                                 type="text"
                                 id="username"
                                 name="username"
                                 placeholder="This is what they will use to log in"
-                                ref={register({ validate: validateCredentials })} 
+                                ref={register({ validate: validateCredentials })}
                                 defaultValue={''}
                                 w='55%'
                                 data-testid='username'
                             />
-                            <FormErrorMessage> 
+                            <FormErrorMessage>
                                 {errors.username && errors.username.message}
                             </FormErrorMessage>
                         </FormControl>
                         <FormControl isInvalid={errors.portfolioname} w='50%'>
                             <FormLabel htmlFor="porfolioname">Portfolio name (optional)</FormLabel>
-                            <Input 
+                            <Input
                                 type="text"
                                 id="porfolioname"
                                 name="porfolioname"
                                 placeholder='Useful for nicknames! ie: Bobby’s portfolio'
-                                ref={register()} 
+                                ref={register()}
                                 defaultValue={''}
                                 w='60%'
                                 data-testid='portName'
                             />
-                            <FormErrorMessage> 
+                            <FormErrorMessage>
                                 {errors.portfolioname && errors.portfolioname.message}
                             </FormErrorMessage>
                         </FormControl>
                         <FormControl isInvalid={errors.password} w='39%'>
                             <FormLabel htmlFor="password">Password</FormLabel>
-                            <Input 
+                            <Input
                                 type="password"
                                 id="password"
                                 name="password"
                                 ref={register({
                                     required: "You must specify a password",
                                     minLength: {
-                                      value: 8,
-                                      message: "Password must have at least 8 characters"
+                                        value: 8,
+                                        message: "Password must have at least 8 characters"
                                     }
-                                  })} 
+                                })}
                                 defaultValue={''}
                                 placeholder={`Your student's password`}
                                 w='55%'
                                 data-testid='password'
                             />
-                            <FormErrorMessage> 
+                            <FormErrorMessage>
                                 {errors.password && errors.password.message}
                             </FormErrorMessage>
                         </FormControl>
                         <FormControl isInvalid={errors.firstname} w='50%'>
                             <FormLabel htmlFor="firstname">First name</FormLabel>
-                            <Input 
+                            <Input
                                 type="text"
                                 id="firstname"
                                 name="firstname"
                                 placeholder={`What is the student's first name?`}
-                                ref={register({ validate: validateCredentials })} 
+                                ref={register({ validate: validateCredentials })}
                                 defaultValue={''}
                                 w='60%'
                                 data-testid='fName'
                             />
-                            <FormErrorMessage> 
+                            <FormErrorMessage>
                                 {errors.firstname && errors.firstname.message}
                             </FormErrorMessage>
                         </FormControl>
                         <FormControl isInvalid={errors.passwordconfirmation} w='39%'>
                             <FormLabel htmlFor="passwordconfirmation">Password (Confirmation)</FormLabel>
-                            <Input 
+                            <Input
                                 type="password"
                                 id="passwordconfirmation"
                                 name="passwordconfirmation"
                                 placeholder={`Confirm your student's password`}
                                 ref={register({
                                     validate: value =>
-                                      value === password.current || "The passwords do not match"
-                                  })} 
+                                        value === password.current || "The passwords do not match"
+                                })}
                                 defaultValue={''}
                                 w='55%'
                                 data-testid='password2'
                             />
-                            <FormErrorMessage> 
+                            <FormErrorMessage>
                                 {errors.passwordconfirmation && errors.passwordconfirmation.message}
                             </FormErrorMessage>
                         </FormControl>
                         <FormControl isInvalid={errors.lastname} w='50%'>
                             <FormLabel htmlFor="lastname">Last name</FormLabel>
-                            <Input 
+                            <Input
                                 type="text"
                                 id="lastname"
                                 name="lastname"
                                 placeholder={`What is the student's last name?`}
-                                ref={register({ validate: validateCredentials })} 
+                                ref={register({ validate: validateCredentials })}
                                 defaultValue={''}
                                 w='60%'
                                 data-testid='lName'
                             />
-                            <FormErrorMessage> 
+                            <FormErrorMessage>
                                 {errors.lastname && errors.lastname.message}
                             </FormErrorMessage>
                         </FormControl>
                         <Text w='60%'></Text>
-                        <Button variantColor="green"w='7vw' type='submit' data-testid='submit'>Submit </Button>
+                        <Button variantColor="green" w='7vw' type='submit' data-testid='submit'>Submit </Button>
                     </Flex>
                 </form>
-            </FormContext>
+
             </Flex>
         </>
     )
@@ -187,10 +185,10 @@ const StudentRegister = props => {
 
 const mapStateToProps = (state) => {
     return {
-      family: state.usersReducer.family,
-      isLoading: state.usersReducer.isLoading,
-      err: state.usersReducer.error
+        family: state.usersReducer.family,
+        isLoading: state.usersReducer.isLoading,
+        err: state.usersReducer.error
     }
-  }
+}
 export default connect(mapStateToProps, { getFamilyName })(StudentRegister);
 // export default StudentRegister;
