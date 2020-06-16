7c29f1335b7a08123a6b460e3140f5669feb3104
Elysia Gabe <elysiagabe@gmail.com>
2020-06-16 16:55:48 -0700

    Updates StudentCard with fix for recent activity

--- my-school/src/Components/Dashboards/StudentCard.js
+++ my-school/src/Components/Dashboards/StudentCard.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import { Flex, Image, IconButton, Heading, Text } from "@chakra-ui/core";
 import placeholder from "../../assets/placeholder_img.png";
-import {connect} from 'react-redux';
+import {connect, useSelector} from 'react-redux';
 import {getAllActivitiesForUser} from '../../Redux/actions/actions-portfolio'
 import moment from 'moment';
 import {useHistory ,useParams} from 'react-router-dom'
 import capitalizeName from '../../utils/capitalizeName'
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
 
 const StudentCard = ({ student, familyName, activities }) => {
   const history = useHistory()
+
+  const [recent, setRecent] = useState({})
 
   const lastActivity = activities.sort((a, b) => b.created_at - a.created_at)
 
@@ -17,6 +21,14 @@ const StudentCard = ({ student, familyName, activities }) => {
   const pushToPortfolio = (studentId) => {
     history.push(`/portfolio/${studentId}`);
   };
+
+  useEffect(() => {
+    axios.get(`https://my-school-v1.herokuapp.com/api/users/${student.id}/activities`)
+    .then(res => {
+      setRecent(res.data[res.data.length - 1])
+    })
+    .catch(err => console.log(err))
+  }, [])
 
   return (
     <Flex width={['90vw', '90vw', '400px', '25vw']} direction="column" border="1px solid #ededed" borderRadius='15px' padding='1%' height={['30vh', '30vh', '30vh', '18vh']} justify='space-evenly'>
@@ -30,8 +42,8 @@ const StudentCard = ({ student, familyName, activities }) => {
 
       </Flex>
       <Flex direction='column'>
-      <Text fontWeight='800' fontSize='.9rem'>LAST ACTIVITY</Text>
-  {lastActivity[0] && <Text>{moment(lastActivity[0].created_at).format('ll').toUpperCase()} SUBMITTED {lastActivity[0].name}</Text>}
+      <Text fontWeight='800' fontSize='.9rem'>RECENT ACTIVITY</Text>
+  {recent && <Text>{moment(recent.created_at).format('ll').toUpperCase()} SUBMITTED {recent.name}</Text>}
       </Flex>
     </Flex>
   );
