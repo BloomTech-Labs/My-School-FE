4e4cbeef9647968df546f56e942f4d6bc5ae6a95
Dylan Collins <dcollins5590@students.mc3.edu>
2020-06-22 17:12:22 -0400

    change color of buttons for settings, made the photo post faster

--- my-school/src/components/settings/UpdateButton.js
+++ my-school/src/components/settings/UpdateButton.js
@@ -20,51 +20,52 @@ const UpdateButton = ({ user, passwordField, renderedUser, image }) => {
           user.username !== "" &&
           user.username !== renderedUser.username &&
           user.name !== renderedUser.name) ||
-        (user.user_type_id === 2 &&
-          passwordField === false &&
-          user.username !== renderedUser.username &&
-          user.username !== "" &&
-          user.name !== "") ||
-        (user.user_type_id === 2 &&
-          passwordField === true &&
-          user.password.length > 7 &&
-          user.password === user.passwordConfirm &&
-          user.name !== "" &&
-          user.username !== "") ||
-        (user.user_type_id === 2 &&
-          passwordField === false &&
-          user.username !== "" &&
-          user.name !== "" &&
-          user.name !== renderedUser.name) ||
-        (user.user_type_id === 2 &&
-          passwordField === false &&
-          user.name !== renderedUser.name &&
-          user.username !== "" &&
-          user.name !== "") ||
-        (user.user_type_id === 1 &&
-          passwordField === true &&
-          user.password.length > 7 &&
-          user.password === user.passwordConfirm &&
-          user.username !== "" &&
-          EmailValidator.validate(user.username) === true) ||
-        (user.user_type_id === 1 &&
-          passwordField === false &&
-          user.username !== renderedUser.username &&
-          EmailValidator.validate(user.username) === true) ||
-        (user.user_type_id === 1 &&
-          passwordField === false &&
-          user.name !== renderedUser.name &&
-          EmailValidator.validate(user.username) === true) ||
-        image
+          (user.user_type_id === 2 &&
+            passwordField === false &&
+            user.username !== renderedUser.username &&
+            user.username !== "" &&
+            user.name !== "") ||
+          (user.user_type_id === 2 &&
+            passwordField === true &&
+            user.password.length > 7 &&
+            user.password === user.passwordConfirm &&
+            user.name !== "" &&
+            user.username !== "") ||
+          (user.user_type_id === 2 &&
+            passwordField === false &&
+            user.username !== "" &&
+            user.name !== "" &&
+            user.name !== renderedUser.name) ||
+          (user.user_type_id === 2 &&
+            passwordField === false &&
+            user.name !== renderedUser.name &&
+            user.username !== "" &&
+            user.name !== "") ||
+          (user.user_type_id === 1 &&
+            passwordField === true &&
+            user.password.length > 7 &&
+            user.password === user.passwordConfirm &&
+            user.username !== "" &&
+            EmailValidator.validate(user.username) === true) ||
+          (user.user_type_id === 1 &&
+            passwordField === false &&
+            user.username !== renderedUser.username &&
+            EmailValidator.validate(user.username) === true) ||
+          (user.user_type_id === 1 &&
+            passwordField === false &&
+            user.name !== renderedUser.name &&
+            EmailValidator.validate(user.username) === true) ||
+          image
           ? false
           : true
       }
-      _focus={{ boxShadow: "outline" }}
-      variant={hover ? "outline" : "solid"}
-      variantColor="btnYellow"
-      ref={btnRef}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
+      textTransform="uppercase"
+      letterSpacing="1.2px"
+      size="xs"
+      mx="4px"
+      variant="outline"
+      variantColor="green"
+      p='1.5rem'
     >
       Update Account
     </Button>
